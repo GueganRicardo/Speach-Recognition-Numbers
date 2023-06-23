3d1791f2b60f0e504e57ffeded8aef183b034673
Raul Sofia <rauljcsofia@gmail.com>
2023-06-23 06:49:35 +0100

    ,nm kn

--- ProjetoATD.m
+++ ProjetoATD.m
@@ -14,6 +14,40 @@ for j = 0:9
         [wave{i+1,j+1} ,fs{i+1,j+1}] =audioread(local_fich);
     end
 end
+
+%% filtrar frequencias de voz do ruido (1000 - 4000 hz)
+filtered_waves = cell(50, 10);
+
+tamJanela = 48000;%testar para várias janelas
+freq = cell(10,1);%ver os picos em diferentes itervalos de freq
+Q1 = cell(10,1);
+Q3 = cell(10,1);
+figure(n_figura);
+n_figura = n_figura + 1;
+for j = 1:10
+    sinais = zeros(tamJanela/2+1, 50);
+    for i = 1:50
+        X = fft(wave{i,j}, tamJanela);
+        X_positivo = X(1:tamJanela/2+1);
+        X_normalizado = abs(X_positivo) / tamJanela;
+        sinais(:,i) = X_normalizado;
+        %X (1:1000, i) = 0; % filtro
+        %filtered_waves{i, j} = ifft(X);
+    end
+    freq{j} = median(sinais, 2);
+    Q1{j} = quantile(sinais, 0.25, 2);
+    Q3{j} = quantile(sinais, 0.75, 2);
+end
+for i = 1:50
+    for j = 1:10
+        subplot(2,5,j);
+        plot(ifft(X));
+        hold on;
+        title(num2str(d-1));
+        xlabel('Frequência (Hz)');
+        ylabel('Amplitude (normalizada)');
+    end
+end
 %% fazer a analise no tempo
 caracteristica = zeros(50,3);
 figure(n_figura);
@@ -57,9 +91,9 @@ end
 
 
 %% dar trim para alinhar, com base na energia de janelas constantes
-%trim_wave = cell(50, 10);
+trim_waves = cell(50, 10);
 janela_sz = 100;
-limiar = 0.05;
+limiar = 0.025;
 
 caracteristica = zeros(50,3);
 figure(n_figura);
@@ -73,7 +107,7 @@ for j = 0:9
         max_amp = max(abs(this_wave));
         norm_wave = this_wave / max_amp;
 
-
+        %lower lim
         energia_janela = 0;
         janela = -1;
         tolerancia = 0;
@@ -90,7 +124,29 @@ for j = 0:9
                 tolerancia = 0;
             end
         end
-        trim_wave = norm_wave(lower_lim:size(norm_wave));
+        
+        %upper_lim
+        energia_janela = 0;
+        janela = -1;
+        tolerancia = 0;
+        while tolerancia < 5
+            janela = janela + 1; %as janelas agora avançam para trás no tempo
+            aux = power(norm_wave(size(this_wave) - ((janela + 1) * janela_sz) + 1 : size(this_wave) - (janela_sz * janela) , 1), 2);
+            energia_janela = sum(aux);
+            if energia_janela > limiar
+                tolerancia = tolerancia + 1;
+                if tolerancia == 1
+                    upper_lim = size(this_wave) - (janela_sz * janela);
+                end
+            else
+                tolerancia = 0;
+            end
+        end
+
+
+
+        trim_wave = norm_wave(lower_lim:upper_lim);
+        trim_waves{i, j + 1} = trim_wave;
         t = linspace(0, 1, size(trim_wave, 1));
         plot(t,trim_wave);
         hold on
@@ -114,10 +170,78 @@ for e = 0:9
 end
 boxplot(energia, eixo_x);
 
+%% plotar a energia em linhas mas em janelas
+
+n_janelas = 50; %tamanho do vetor / numero de janelas = numero de samples em cada janela. assim todas as janelas têm o mesmo tamanho relativo. possivelmente sera necessario dar trim do vetor para detetar quando começa e acaba o som
+new_wave = trim_waves;
+energias_janelas = zeros(50, 10, n_janelas);
+
+for j = 1:n_janelas
+    % cada digito
+    for e = 0:9
+        % cada sinal
+        for s = 1:50
+            this_wave = new_wave{s, e + 1};
+            max_amp = max(this_wave);
+            %disp(max_amp);
+            this_wave = this_wave / max_amp;
+            window_sz = fix(size(this_wave, 1) / n_janelas);
+            lower_lim = window_sz * (j - 1) + 1;
+            upper_lim = window_sz * j;
+            aux = power(this_wave(lower_lim:upper_lim, 1), 2);
+            energia(s, e + 1) = sum(aux);
+            %disp(energia(s, e + 1))
+            energias_janelas(s, e + 1, j) = energia(s, e + 1);
+        end 
+    end
+    %plot(energia, eixo_x);
+    %ylim([0 200]);
+end
+
+figure(n_figura);
+n_figura = n_figura + 1;
+for e = 0:9
+    subplot(3,4,e+1);
+    for s = 1:50
+        plot(1:n_janelas, squeeze(energias_janelas(s, e + 1, :)))
+        hold on
+    end
+    title(int2str(e))
+    xlabel("Tempo")
+    ylabel("Energia")
+    ylim([0 200])
+end
+
+% plotar as energias normalizadas (e_parcial_1 + e_parcial_2 + ... +
+% e_parcial_n = e_total
+figure(n_figura);
+n_figura = n_figura + 1;
+for e = 0:9
+    subplot(2,5,e+1);
+    for s = 1:50
+        plot(1:n_janelas, squeeze(energias_janelas(s, e + 1, :)))
+        hold on
+    end
+    title(int2str(e))
+    xlabel("Tempo")
+    ylabel("Amplitude")
+end
+    
+
+
+        
+
+%% boxplot do racio
+figure(n_figura);
+n_figura = n_figura + 1;
+racios = energias_metades(1:50, 1:10, 1) ./ energias_metades(1:50, 1:10, 2);
+boxplot(racios, eixo_x);
+ylim([0 30]);
+
 %% plotar a energia em boxplots mas em janelas
 
 n_janelas = 50; %tamanho do vetor / numero de janelas = numero de samples em cada janela. assim todas as janelas têm o mesmo tamanho relativo. possivelmente sera necessario dar trim do vetor para detetar quando começa e acaba o som
-new_wave = wave;
+new_wave = trim_waves;
 energias_metades = zeros(50, 10, n_janelas);
 for j = 1:n_janelas
     figure(n_figura);
